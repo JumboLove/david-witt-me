c2199621721ec75e710fff8ddbbc2a2add496d1e
David Witt <midknight55@gmail.com>
2023-07-10 11:16:45 -0700

    Setup basic version of resource filters

---
+++ apps/site-astro/src/queries/filters/resource.ts
@@ -0,0 +1,18 @@
+import { groq } from "astro-sanity";
+
+interface Filter {
+  title: string;
+  query: string;
+}
+
+export const filters: Map<string, Filter> = new Map();
+
+filters.set("books", {
+  title: "Books",
+  query: groq`*[count((tags[]->slug.current)[@ in ["nonfiction", "fiction"]]) > 0 && _type == "resource" && isVisible == true]`,
+});
+
+filters.set("webdev", {
+  title: "Web Dev",
+  query: groq`*[count((tags[]->slug.current)[@ in ["typescript", "tailwind", "astro", "sanity", "react", "remix", "commerce-cloud"]]) > 0 && _type == "resource" && isVisible == true]`,
+});
