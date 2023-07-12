5a082a0daf89e759f0729a0b059fd0426d901b26
David Witt <midknight55@gmail.com>
2023-07-12 12:48:58 -0700

    Updated Server Embed and registry to work with URLs

--- packages/sanity-astro-embeds/src/embedRegistry.tsx
+++ packages/sanity-astro-embeds/src/embedRegistry.tsx
@@ -5,6 +5,7 @@ import getYouTubeID from "get-youtube-id";
 export type EmbedProvider<TProps> = {
   title: string;
   regexp: RegExp;
+  getRenderProps: (urlOrId: string) => { id: string };
   render: FunctionComponent<TProps>;
 };
 
@@ -16,23 +17,33 @@ export const embedRegistry: EmbedRegistry = {
   twitter: {
     title: "Twitter",
     regexp: /^https?:\/\/twitter\.com/,
-    render: ({ url, id, ...props }) =>
-      id ? (
-        <Tweet id={id} {...props} />
-      ) : (
-        url && <Tweet tweetLink={url} {...props} />
-      ),
+    getRenderProps: (urlOrId: string) => {
+      const tweetIdRegex = /^https?:\/\/twitter\.com\/([\w-]+\/status\/\d+)/;
+      const match = urlOrId.match(tweetIdRegex);
+      const id = match ? match[1] : "";
+      return { id };
+    },
+    render: ({ id, ...props }) =>
+      id ? <Tweet tweetLink={id} {...props} /> : null,
   },
   vimeo: {
     title: "Vimeo",
     regexp: /^https?:\/\/vimeo\.com/,
-    render: ({ url, ...props }) =>
-      url ? <Vimeo url={url} {...props} /> : null,
+    getRenderProps: (urlOrId: string) => {
+      const id = urlOrId.replace(/^https?:\/\/vimeo\.com\/(\d+)$/, "$1");
+      return { id };
+    },
+    render: ({ id, ...props }) =>
+      id ? <Vimeo vimeoId={id} {...props} /> : null,
   },
   youTube: {
     title: "YouTube",
     regexp:
       /^https?:\/\/(?:www\.)?youtu(?:be\.com\/(?:watch\?v=|embed\/)|\.be\/)([\w\-]+)(?:$|\&|\?)/,
+    getRenderProps: (urlOrId: string) => {
+      const id = getYouTubeID(urlOrId) || "";
+      return { id };
+    },
     render: ({ id, ...props }) =>
       id ? <YouTube youTubeId={id} {...props} /> : null,
   },
