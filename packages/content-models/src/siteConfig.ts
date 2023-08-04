ea4008d3fa2232d010812845a787b09be341de3a
David Witt <midknight55@gmail.com>
2023-08-04 14:32:58 -0700

    Added a Site Config sanity object

---
+++ packages/content-models/src/siteConfig.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from "sanity";
+import { CogIcon } from "@sanity/icons";
+import { z } from "zod";
+import * as S from "sanity-zod-types";
+import { formatDate } from "./util";
+
+export const siteConfigSanityDefinition = defineType({
+  name: "siteConfig",
+  title: "Site Config",
+  type: "document",
+  icon: CogIcon,
+  fields: [
+    defineField({
+      name: "featuredBook",
+      title: "Featured Book",
+      description: "Book highlighted on library page",
+      type: "reference",
+      to: { type: "resource" },
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+  preview: {
+    select: { updated: "_updatedAt" },
+    prepare(selection) {
+      const { updated } = selection;
+      return {
+        title: "Active Site Configuration",
+        subtitle: `Last edited: ${formatDate(updated)}`,
+      };
+    },
+  },
+});
+
+export const SiteConfig = S.Document.extend({
+  featuredBook: S.Reference.nullable(),
+});
+
+export type SiteConfig = z.infer<typeof SiteConfig>;
